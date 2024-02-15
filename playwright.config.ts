106df9985b478968626251bbd10a16a13230d00a
Konrad Skurski <skurskikonrad@gmail.com>
2024-02-15 19:00:16 +0100

    chore: init playwright

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: { timeout: 10_000 },
+  fullyParallel: true,
+  retries: 0,
+  workers: undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://spartantest-puppies.herokuapp.com',
+    actionTimeout: 0,
+    trace: 'on',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+
+});
